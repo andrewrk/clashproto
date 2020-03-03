6c680fc999642ca7167f6d33010382c854ad640d
Andrew Kelley <andrew@ziglang.org>
2020-03-03 17:37:58 -0500

    update to latest zig

--- build.zig
+++ build.zig
@@ -1,13 +1,13 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    const target = b.standardTargetOptions(null);
+    const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("clashproto", "src/main.zig");
     exe.addCSourceFile("deps/stb_image.c", &[_][]const u8{"-std=c99"});
     exe.addIncludeDir("deps");
     exe.setBuildMode(mode);
-    exe.setTheTarget(target);
+    exe.setTarget(target);
     exe.linkSystemLibrary("SDL2");
     exe.linkLibC();
     exe.install();
